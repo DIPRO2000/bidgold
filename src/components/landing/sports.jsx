750c77cbd12f29a3dbca82f42b6f63c2e2ef213b
Stuti Sinha <stutisinha712@gmail.com>
2025-04-04 22:02:46 +0530

    Enhance Sports component layout: update max-width, center text, and improve grid responsiveness for better display on various screen sizes.

--- src/components/landing/sports.jsx
+++ src/components/landing/sports.jsx
@@ -12,16 +12,16 @@ function Sports() {
   ];
 
   return (
-    <div className="bg-[#47B67C] rounded-md p-6 w-full max-w-6xl mx-auto shadow-lg">
-      <h2 className="text-white text-lg font-semibold mb-4">
+    <div className="bg-[#47B67C] rounded-md p-6 w-full max-w-screen-lg mx-auto shadow-lg">
+      <h2 className="text-white text-lg font-semibold mb-4 text-center sm:text-left">
         Fixtures of all Upcoming Matches
       </h2>
-      <div className="grid grid-cols-5 gap-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-6">
         {sports.map((sport) => (
           <button
             key={sport.id}
             onClick={() => setActiveSport(sport.id)}
-            className={`flex flex-col items-center justify-center px-4 py-6 min-h-[120px] rounded-lg  shadow-black transition
+            className={`w-full flex flex-col items-center justify-center px-4 py-6 min-h-[120px] rounded-lg shadow-black transition
               ${activeSport === sport.id
                 ? 'bg-[#208C53] text-white shadow-inner'
                 : 'bg-white text-black shadow-md hover:bg-gray-200 dark:bg-[#7C7C7C]'}
